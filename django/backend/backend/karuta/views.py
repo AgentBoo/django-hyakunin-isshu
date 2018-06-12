3c8a30cd9ce1c25ccb301fdbaa177184f205002b
agentboo <zuzi.han@gmail.com>
2018-06-12 11:19:38 -0400

    files before merger with mac files

--- django/backend/backend/karuta/views.py
+++ django/backend/backend/karuta/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from backend.karuta.models import Poem
+from backend.karuta.serializers import PoemSerializer 
+from rest_framework import viewsets 
 
 # Create your views here.
+class PoemViewSet(viewsets.ModelViewSet):
+	'''
+	API endpoint that allows poems to be viewed or edited
+	''' 
+	queryset = Poem.objects.all().order_by('numeral')
+	serializer_class = PoemSerializer
