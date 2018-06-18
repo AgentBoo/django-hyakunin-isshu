d7a6ab8ed95e90c8c20a7416f12f9bf30c74bc40
agentboo <zuzi.han@gmail.com>
2018-06-18 17:53:56 -0400

    predeploy

--- django/backend/backend/local_settings.py
+++ django/backend/backend/local_settings.py
@@ -7,25 +7,25 @@
 import os
 
 # DEVELOPMENT 
-DEBUG = True
+# DEBUG = True
 
-DJANGO_SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'xw9s+3ykue_)evq%4r=y=!h$7m^_(0%x0n7413xte=z56m)uiyhahaheeheehohorose2018')
+# DJANGO_SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'xw9s+3ykue_)evq%4r=y=!h$7m^_(0%x0n7413xte=z56m)uiyhahaheeheehohorose2018')
 
-DATABASE_NAME = 'poems'
-DATABASE_ROLE = 'poems'
-DATABASE_PASSWORD = 'poems'
-DATABASE_PORT = '5432'
+# DATABASE_NAME = 'poems'
+# DATABASE_ROLE = 'poems'
+# DATABASE_PASSWORD = 'poems'
+# DATABASE_PORT = '5432'
 
 
-LOCAL_HOSTS = []
+# LOCAL_HOSTS = []
 
-LOCAL_WHITELIST = (
-    'localhost:3000',
-)
+# LOCAL_WHITELIST = (
+#     'localhost:3000',
+# )
 
-LOCAL_TRUSTED_ORIGINS = (
-    'localhost:3000',
-)
+# LOCAL_TRUSTED_ORIGINS = (
+#     'localhost:3000',
+# )
 
 
 # Superuser 
@@ -33,11 +33,15 @@ LOCAL_TRUSTED_ORIGINS = (
 # --username admin 
 # --password hakuoro++++
 
+# URL 
+# https://fierce-hollows-19151.herokuapp.com/
+# https://git.heroku.com/fierce-hollows-19151.git
+
 # List of env variables: DJANGO_DEBUG, DJANGO_SECRET_KEY, APP_URL, REACT_APP_DJANGO_API_URL(for React)
 # In react, only variables starting with REACT_APP_ are imported from .env files
 # Because I am running npm run build locally, I need to make sure REACT_APP_DJANGO_API_URL is exported
 
-'''
+
 # PRODUCTION
 # For production, see https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -48,9 +52,8 @@ if not DEBUG:
 
 	DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
-	LOCAL_HOSTS = []
+	LOCAL_HOSTS = [ os.environ['APP_URL'] ]
 
 	LOCAL_WHITELIST = os.getenv('APP_URL', 'localhost:3000')
 
 	LOCAL_TRUSTED_ORIGINS = os.getenv('APP_URL', 'localhost:3000')
-'''
