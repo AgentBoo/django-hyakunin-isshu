9daf9654fdf5b56884e55dc9de2ef7e8cabb850a
agentboo <zuzi.han@gmail.com>
2018-06-18 21:22:13 -0400

    changes to lab-book and settings

--- django/backend/backend/local_settings.py
+++ django/backend/backend/local_settings.py
@@ -12,6 +12,11 @@ DEBUG = True
 
 DJANGO_SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'xw9s+3ykue_)evq%4r=y=!h$7m^_(0%x0n7413xte=z56m)uiyhahaheeheehohorose2018')
 
+DATABASE_NAME = 'poems'
+DATABASE_ROLE = 'poems'
+DATABASE_PASSWORD = 'poems'
+DATABASE_PORT = '5432'
+
 LOCAL_DATABASE = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -23,36 +28,19 @@ LOCAL_DATABASE = {
     }
 }
 
-DATABASE_NAME = 'poems'
-DATABASE_ROLE = 'poems'
-DATABASE_PASSWORD = 'poems'
-DATABASE_PORT = '5432'
-
-
 LOCAL_HOSTS = []
 
 LOCAL_WHITELIST = (
     'localhost:3000',
+    'localhost:8000'
 )
 
 LOCAL_TRUSTED_ORIGINS = (
     'localhost:3000',
+    'localhost:8000'
 )
 '''
 
-# Superuser 
-# --email chibi.chan@seznam.cz 
-# --username admin 
-# --password hakuoro++++
-
-# URL 
-# https://fierce-hollows-19151.herokuapp.com/
-# https://git.heroku.com/fierce-hollows-19151.git
-
-# List of env variables: DJANGO_DEBUG, DJANGO_SECRET_KEY, APP_URL, REACT_APP_DJANGO_API_URL(for React)
-# In react, only variables starting with REACT_APP_ are imported from .env files
-# Because I am running npm run build locally, I need to make sure REACT_APP_DJANGO_API_URL is exported
-# heroku config:set WSGI_PATH=
 
 # PRODUCTION
 # For production, see https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -61,15 +49,16 @@ LOCAL_TRUSTED_ORIGINS = (
 import dj_database_url 
 
 
-DJANGO_SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkagbboombboommomoland2018')
+DJANGO_SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 LOCAL_DATABASE = {
     'default': dj_database_url.config(conn_max_age=600)
 }
 
-# LOCAL_HOSTS = [ 'https://fierce-hollows-19151.herokuapp.com', os.getenv('APP_URL','localhost:8000') ]
-LOCAL_HOSTS = ['*']
+LOCAL_HOSTS = [ os.getenv('ROOT_URL', '*'), ]
 
-LOCAL_WHITELIST = os.getenv('APP_URL', 'localhost:3000')
+LOCAL_WHITELIST = [ os.getenv('ROOT_URL', 'localhost:3000'), ]
 
-LOCAL_TRUSTED_ORIGINS = os.getenv('APP_URL', 'localhost:3000')
+LOCAL_TRUSTED_ORIGINS = [ os.getenv('ROOT_URL', 'localhost:3000'), ]
+
+
