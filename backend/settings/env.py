b598a5227c12563c091c2566b0d07bfd60abee10
agentboo <zuzi.han@gmail.com>
2019-03-18 19:02:48 -0400

    refactored settings

--- backend/settings/env.py
+++
@@ -1,7 +0,0 @@
-import os 
-
-ENV      = os.getenv('DJANGO_ENVIRONMENT', 'production')
-DEV_ENV  = ENV == 'development'
-TEST_ENV = ENV == 'staging'
-PROD_ENV = ENV == 'production'
-print(f'DJANGO_ENV: {ENV}')
