85c54507cac69b566cecfe303e4cc7dd1eb0d49c
agentboo <zuzi.han@gmail.com>
2019-03-26 19:26:24 -0400

    mobx store, localization

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,37 +1,19 @@
-import React, { Component } from 'react';
-import {getURL, fetchData} from "./../utils/fetch";
+import React, { Component } from "react";
+import { AppStore } from "./AppStore";
+
+const store = new AppStore();
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      data: null 
-    }
-  }
-
-  componentDidMount(){
-    const url = getURL('poems')
-    const next = resjson => this.setState({ data: resjson })
-    const success = status => console.log(status) 
-    const failure = error => console.error(error)
-    return fetchData(url, {}, next, success, failure)
-  }
-
   render() {
-    console.log(this.state.data)
     return (
-      <div className="App">
-        <main className="App-main">
-          <p>Message</p>
-          <hr/>
-          <div className="App-guts">
-            Hello, World!
-          </div>
-          <footer className="App-footer">
-            <a href="#">10-20</a>
-          </footer>
+      <div>
+        <main>
+          <h4>ohi</h4>
+          <button onClick={() => store.localize("eng")}>ENG</button>
+          <button onClick={() => store.localize("jap")}>JAP</button>
+          <button onClick={() => store.localize("rom")}>ROM</button>
         </main>
-        <aside className="App-side">ohi</aside>
+        <aside>ohi</aside>
       </div>
     );
   }
