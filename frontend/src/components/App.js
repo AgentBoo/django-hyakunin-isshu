0d1c27c21eee2f4b0a79ebb7e8e1f0f92370b17b
agentboo <zuzi.han@gmail.com>
2019-03-26 22:44:34 -0400

    router, list and detail view

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,22 +1,34 @@
 import React, { Component } from "react";
+import { observer } from "mobx-react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { AppStore } from "./AppStore";
+import { List } from "./List";
+import { Detail } from "./Detail";
 
-const store = new AppStore();
+export const store = new AppStore();
 
 class App extends Component {
+  componentDidMount() {
+    store.fetch();
+  }
+
   render() {
     return (
-      <div>
+      <Router>
         <main>
-          <h4>ohi</h4>
-          <button onClick={() => store.localize("eng")}>ENG</button>
-          <button onClick={() => store.localize("jap")}>JAP</button>
-          <button onClick={() => store.localize("rom")}>ROM</button>
+          <button onClick={() => store.setLocale("eng")}>ENG</button>
+          <button onClick={() => store.setLocale("jap")}>JAP</button>
+          <button onClick={() => store.setLocale("rom")}>ROM</button>
+          <section>
+            <Route exact strict path="/" component={List} />
+            <Route exact strict path="/:id" component={Detail} />
+          </section>
         </main>
-        <aside>ohi</aside>
-      </div>
+      </Router>
     );
   }
 }
+
+App = observer(App);
 
 export default App;
