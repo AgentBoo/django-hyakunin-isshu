a49af0340c276e3144ed9f41a200f0381da1d0a2
agentboo <zuzi.han@gmail.com>
2019-04-25 12:02:57 -0400

    added font awesome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import { Provider } from "mobx-react";
 import { AppStore } from "./store/AppStore";
+import { initIconLibrary } from "./config/icons";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Splash } from "./components/shared";
 import { List, Detail, NotFound } from "./components/pages";
 
-export const store = new AppStore();
+const store = new AppStore();
+
+initIconLibrary()
 
 class App extends Component {
 	componentDidMount() {
+		/* prettier-ignore */
 		store.dataStore.retrieve("poems").then(resjson => 
 			store.dataStore.setCollection(resjson)
 		);
@@ -17,7 +21,7 @@ class App extends Component {
 	render() {
 		return (
 			<Provider store={store}>
-				<div id="app">
+				<div id="app-root">
 					<Splash />
 					<Router>
 						<Switch>
