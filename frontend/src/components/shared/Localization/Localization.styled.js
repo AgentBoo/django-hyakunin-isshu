1b549808482b29dfad56ff003432faefdc6ad4e0
agentboo <zuzi.han@gmail.com>
2019-04-25 12:01:31 -0400

    styled localization

--- frontend/src/components/shared/Localization/Localization.styled.js
+++ frontend/src/components/shared/Localization/Localization.styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import styles from "./../../../config/styles";
 import { Button } from "./../Button.component";
 
 export const StyledSection = styled.section`
@@ -6,14 +7,23 @@ export const StyledSection = styled.section`
 	align-items: baseline;
 	flex-flow: row nowrap;
 	justify-content: flex-end;
+
+	@media screen and (max-width: ${styles.breakpoint.xs}px) {
+		justify-content: flex-start;
+	}
 `;
 
 export const LocaleButton = styled(Button)`
-	font-weight: ${props => (props.active ? "bold" : "normal")}
+	color: ${props => (props.active ? styles.base.accent : styles.base.secondary)};
+	font-weight: bold;
 	margin-left: calc(1em + 1vw);
-	
-	&:hover{
-		color: grey;
+
+	&:hover {
+		color: ${styles.light.primary};
+	}
+
+	@media screen and (max-width: ${styles.breakpoint.xs}px) {
+		margin-left: 0;
+		margin-right: calc(1em + 1vw);
 	}
 `;
- 
