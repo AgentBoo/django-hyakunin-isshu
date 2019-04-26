6c4161c030ecb5ba32655294e43d567f63a30020
agentboo <zuzi.han@gmail.com>
2019-04-26 15:32:13 -0400

    List page component now fetches data on mount

--- frontend/src/components/pages/List/Page.component.js
+++ frontend/src/components/pages/List/Page.component.js
@@ -1,22 +1,43 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
+import { inject, observer } from "mobx-react";
 import { Header, Localization, Main } from "./../../shared";
 import { SearchForm } from "./SearchForm.component";
 import { ResultBar } from "./ResultBar.component";
 import { Poems } from "./Poems.component";
 import { Pagination } from "./Pagination.component";
 
-const ListPage = () => (
-	<Fragment>
-		<Header>
-			<SearchForm />
-			<Localization />
-		</Header>
-		<Main>
-			<ResultBar />
-			<Poems />
-			<Pagination />
-		</Main>
-	</Fragment>
-);
+class ListPage extends Component {
+	componentDidMount() {
+		const datastoreIsEmpty = this.props.datastore.isEmpty;
+
+		if (datastoreIsEmpty) {
+			const { datastore } = this.props;
+
+			datastore
+				.retrieve("poems")
+				.then(resjson => this.props.datastore.setCollection(resjson));
+		}
+	}
+
+	render() {
+		return (
+			<Fragment>
+				<Header>
+					<SearchForm />
+					<Localization />
+				</Header>
+				<Main>
+					<ResultBar />
+					<Poems />
+					<Pagination />
+				</Main>
+			</Fragment>
+		);
+	}
+}
+
+ListPage = inject(provider => ({
+	datastore: provider.store.dataStore
+}))(observer(ListPage));
 
 export default ListPage;
